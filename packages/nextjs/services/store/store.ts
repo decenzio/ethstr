c0041c8ac45ec53f8372be3efc4a7cd2143beb83
romispectrum <dev@romispectrum.com>
2025-10-22 18:43:01 +0100

    improve store.ts

--- packages/nextjs/services/store/store.ts
+++ packages/nextjs/services/store/store.ts
@@ -1,3 +1,4 @@
+import { createPublicClient } from "viem";
 import { create } from "zustand";
 import scaffoldConfig from "~~/scaffold.config";
 import { ChainWithAttributes, NETWORKS_EXTRA_DATA } from "~~/utils/scaffold-eth";
@@ -20,6 +21,16 @@ type GlobalState = {
   setIsNativeCurrencyFetching: (newIsNativeCurrencyFetching: boolean) => void;
   targetNetwork: ChainWithAttributes;
   setTargetNetwork: (newTargetNetwork: ChainWithAttributes) => void;
+  walletAddress: string | null;
+  setWalletAddress: (walletAddress: string | null) => void;
+  nPubkey: string;
+  setNPubKey: (nPubkey: string) => void;
+  publicClient: ReturnType<typeof createPublicClient> | any;
+  evmAccount: any;
+  bundlerClient: any;
+  setPublicClient: (publicClient: ReturnType<typeof createPublicClient> | any) => void;
+  setBundlerClient: (publicClient: ReturnType<any> | any) => void;
+  setEvmAccount: (publicClient: any) => void;
 };
 
 export const useGlobalState = create<GlobalState>(set => ({
@@ -27,6 +38,11 @@ export const useGlobalState = create<GlobalState>(set => ({
     price: 0,
     isFetching: true,
   },
+  walletAddress: null,
+  nPubkey: "",
+  publicClient: null,
+  evmAccount: null,
+  bundlerClient: null,
   setNativeCurrencyPrice: (newValue: number): void =>
     set(state => ({ nativeCurrency: { ...state.nativeCurrency, price: newValue } })),
   setIsNativeCurrencyFetching: (newValue: boolean): void =>
@@ -36,4 +52,9 @@ export const useGlobalState = create<GlobalState>(set => ({
     ...NETWORKS_EXTRA_DATA[scaffoldConfig.targetNetworks[0].id],
   },
   setTargetNetwork: (newTargetNetwork: ChainWithAttributes) => set(() => ({ targetNetwork: newTargetNetwork })),
+  setWalletAddress: (walletAddress: string | null) => set(() => ({ walletAddress })),
+  setNPubKey: (nPubkey: string) => set(() => ({ nPubkey })),
+  setPublicClient: (publicClient: ReturnType<typeof createPublicClient> | null) => set(() => ({ publicClient })),
+  setEvmAccount: (evmAccount: ReturnType<any>) => set(() => ({ evmAccount })),
+  setBundlerClient: (bundlerClient: ReturnType<any>) => set(() => ({ bundlerClient })),
 }));
